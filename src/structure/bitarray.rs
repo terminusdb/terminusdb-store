c47f9eae61a2c48a205b028dc235a804006fb88a
Peter <p.w@semantechs.co.uk>
2019-09-06 12:25:12 +0100

    adding 'dyn' where appropriate to resolve deprecated

--- src/structure/bitarray.rs
+++ src/structure/bitarray.rs
@@ -118,7 +118,7 @@ where W: 'static+AsyncWrite {
         }
     }
 
-    fn flush_current(self) -> Box<Future<Item=BitArrayFileBuilder<W>, Error=std::io::Error>> {
+    fn flush_current(self) -> Box<dyn Future<Item=BitArrayFileBuilder<W>, Error=std::io::Error>> {
         let count = self.count;
         Box::new(tokio::io::write_all(self.bit_output, vec![self.current_byte])
                  .map(move |(w,_)| BitArrayFileBuilder {
@@ -129,7 +129,7 @@ where W: 'static+AsyncWrite {
                  }))
     }
 
-    pub fn push(mut self, bit: bool) -> Box<Future<Item=BitArrayFileBuilder<W>, Error=std::io::Error>> {
+    pub fn push(mut self, bit: bool) -> Box<dyn Future<Item=BitArrayFileBuilder<W>, Error=std::io::Error>> {
         let mut b = match bit { true => 128, false => 0 };
         b >>= self.current_bit_pos;
         self.current_byte |= b;
@@ -144,7 +144,7 @@ where W: 'static+AsyncWrite {
         }
     }
 
-    pub fn push_all<S:'static+Stream<Item=bool,Error=std::io::Error>>(self, stream: S) -> Box<Future<Item=BitArrayFileBuilder<W>,Error=std::io::Error>> {
+    pub fn push_all<S:'static+Stream<Item=bool,Error=std::io::Error>>(self, stream: S) -> Box<dyn Future<Item=BitArrayFileBuilder<W>,Error=std::io::Error>> {
         Box::new(stream.fold(self, |builder, bit| builder.push(bit)))
     }
 
@@ -155,7 +155,7 @@ where W: 'static+AsyncWrite {
 
     pub fn finalize(self) -> impl Future<Item=W, Error=std::io::Error> {
         let count = self.count;
-        let flush_current: Box<Future<Item=BitArrayFileBuilder<W>,Error=std::io::Error>> =
+        let flush_current: Box<dyn Future<Item=BitArrayFileBuilder<W>,Error=std::io::Error>> =
             if count % 8 == 0 {
                 Box::new(future::ok(self))
             } else {
