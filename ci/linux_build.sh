1793ad78e692e514ee3b1d07c17da1072d236154
Robin de Rooij <rderooij685@gmail.com>
2020-03-26 00:02:51 +0100

    Fix missing doc rustup component
    Temporary fix until it gets added

--- ci/linux_build.sh
+++ ci/linux_build.sh
@@ -4,7 +4,6 @@ export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-c
 cargo clean
 cargo build --verbose $CARGO_OPTIONS
 cargo test --verbose $CARGO_OPTIONS
-cargo fmt -- --check
 zip -0 ccov.zip `find . \( -name "terminus*.gc*" \) -print`;
 ./grcov ccov.zip -s . -t lcov --llvm --branch --ignore-not-existing --ignore "/*" -o lcov.info;
 bash <(curl -s https://codecov.io/bash) -f lcov.info;
--- ci/osx_build.sh
+++ ci/osx_build.sh
@@ -2,4 +2,3 @@
 cargo clean
 cargo build --verbose $CARGO_OPTIONS
 cargo test --verbose $CARGO_OPTIONS
-cargo fmt -- --check
