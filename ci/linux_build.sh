e958e56e411fd2d79e90e1d627c001cc26d99780
Robin de Rooij <rderooij685@gmail.com>
2021-01-27 10:23:18 +0100

    ci: add code coverage

--- ci/linux_build.sh
+++ ci/linux_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export CARGO_INCREMENTAL=0
-export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Cinline-threshold=0 -Clink-dead-code -Coverflow-checks=off -Zno-landing-pads"
-cargo clean
+export RUSTFLAGS="-Zinstrument-coverage -Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests -Cpanic=abort"
+curl -L https://github.com/mozilla/grcov/releases/latest/download/grcov-linux-x86_64.tar.bz2 | tar jxf -
 cargo build --verbose $CARGO_OPTIONS
 cargo test --verbose $CARGO_OPTIONS
 zip -0 ccov.zip `find . \( -name "terminus*.gc*" \) -print`;
