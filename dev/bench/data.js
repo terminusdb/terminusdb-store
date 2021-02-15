8c7e5fba4710d36b1e22e095a8b050164bd814ae
github-action-benchmark <github@users.noreply.github.com>
2021-02-15 17:28:04 +0000

    add Rust Benchmark (cargo) benchmark result for fbc665b7e34901ffb2de5a4dc693cd28a9b2ac5c

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1613410084168,
+  "repoUrl": "https://github.com/terminusdb/terminusdb-store",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rderooij685@gmail.com",
+            "name": "Robin de Rooij",
+            "username": "rrooij"
+          },
+          "committer": {
+            "email": "rderooij685@gmail.com",
+            "name": "Robin de Rooij",
+            "username": "rrooij"
+          },
+          "distinct": true,
+          "id": "fbc665b7e34901ffb2de5a4dc693cd28a9b2ac5c",
+          "message": "add benchmarking",
+          "timestamp": "2021-02-15T17:53:12+01:00",
+          "tree_id": "09cf9bb975962a8d677197729d3b9cfc62c70518",
+          "url": "https://github.com/terminusdb/terminusdb-store/commit/fbc665b7e34901ffb2de5a4dc693cd28a9b2ac5c"
+        },
+        "date": 1613410080703,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_add_string_triple",
+            "value": 385,
+            "range": "± 45",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
