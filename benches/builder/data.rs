84b898e2657e8bd2f9fea4e18e843ab5bb00701c
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-02-15 22:13:33 +0100

    add builder benchmark tests

---
+++ benches/builder/data.rs
@@ -0,0 +1,71 @@
+use rand::distributions::Alphanumeric;
+use rand::prelude::*;
+use std::iter;
+use terminus_store::layer::StringTriple;
+
+fn random_string<R: Rng>(rand: &mut R, len_min: usize, len_max: usize) -> String {
+    let len: usize = rand.gen_range(len_min, len_max);
+    iter::repeat(())
+        .map(|_| rand.sample(Alphanumeric))
+        .take(len)
+        .collect()
+}
+
+pub struct TestData<R: Rng> {
+    nodes: Vec<String>,
+    predicates: Vec<String>,
+    values: Vec<String>,
+    rand: R,
+}
+
+impl<R: Rng> TestData<R> {
+    pub fn new(
+        mut rand: R,
+        num_nodes: usize,
+        num_predicates: usize,
+        num_values: usize,
+    ) -> TestData<R> {
+        let mut nodes = Vec::with_capacity(num_nodes);
+        let mut predicates = Vec::with_capacity(num_predicates);
+        let mut values = Vec::with_capacity(num_values);
+
+        for _ in 0..num_nodes {
+            nodes.push(random_string(&mut rand, 5, 50));
+        }
+
+        for _ in 0..num_predicates {
+            predicates.push(random_string(&mut rand, 5, 50));
+        }
+
+        for _ in 0..num_values {
+            values.push(random_string(&mut rand, 5, 5000));
+        }
+
+        TestData {
+            nodes,
+            predicates,
+            values,
+            rand,
+        }
+    }
+
+    pub fn random_triple(&mut self) -> StringTriple {
+        let subject_ix = self.rand.gen_range(0, self.nodes.len());
+        let predicate_ix = self.rand.gen_range(0, self.predicates.len());
+        if self.rand.gen() {
+            let object_ix = self.rand.gen_range(0, self.nodes.len());
+            StringTriple::new_node(
+                &self.nodes[subject_ix],
+                &self.predicates[predicate_ix],
+                &self.nodes[object_ix],
+            )
+        } else {
+            let object_ix = self.rand.gen_range(0, self.values.len());
+            StringTriple::new_value(
+                &self.nodes[subject_ix],
+                &self.predicates[predicate_ix],
+                &self.values[object_ix],
+            )
+        }
+    }
+}
---
+++ benches/builder/main.rs
@@ -0,0 +1,118 @@
+#![feature(test)]
+extern crate test;
+mod data;
+
+use rand::prelude::*;
+use tempfile::tempdir;
+use terminus_store;
+use test::Bencher;
+
+use data::*;
+
+#[bench]
+fn build_empty_base_layer(b: &mut Bencher) {
+    let dir = tempdir().unwrap();
+    let store = terminus_store::open_sync_directory_store(dir.path());
+
+    b.iter(|| {
+        let builder = store.create_base_layer().unwrap();
+        builder.commit().unwrap();
+    });
+}
+
+#[bench]
+fn build_base_layer_1000(b: &mut Bencher) {
+    let dir = tempdir().unwrap();
+    let store = terminus_store::open_sync_directory_store(dir.path());
+
+    let seed = b"the quick brown fox jumped over ";
+    let rand = StdRng::from_seed(*seed);
+    let mut data = TestData::new(rand, 100, 25, 500);
+
+    let num_triples = 1000;
+    let mut triples = Vec::with_capacity(num_triples);
+    for _ in 0..num_triples {
+        triples.push(data.random_triple());
+    }
+    b.iter(|| {
+        let builder = store.create_base_layer().unwrap();
+
+        for triple in triples.iter() {
+            builder.add_string_triple(triple.clone()).unwrap();
+        }
+
+        let _base_layer = builder.commit().unwrap();
+    });
+}
+
+#[bench]
+fn build_empty_child_layer_on_empty_base_layer(b: &mut Bencher) {
+    let dir = tempdir().unwrap();
+    let store = terminus_store::open_sync_directory_store(dir.path());
+    let builder = store.create_base_layer().unwrap();
+    let base_layer = builder.commit().unwrap();
+
+    b.iter(|| {
+        let builder = base_layer.open_write().unwrap();
+        builder.commit().unwrap();
+    });
+}
+
+#[bench]
+fn build_nonempty_child_layer_on_empty_base_layer(b: &mut Bencher) {
+    let dir = tempdir().unwrap();
+    let store = terminus_store::open_sync_directory_store(dir.path());
+    let builder = store.create_base_layer().unwrap();
+    let base_layer = builder.commit().unwrap();
+
+    let seed = b"the quick brown fox jumped over ";
+    let rand = StdRng::from_seed(*seed);
+    let mut data = TestData::new(rand, 100, 25, 500);
+
+    let num_triples = 1000;
+    let mut triples = Vec::with_capacity(num_triples);
+    for _ in 0..num_triples {
+        triples.push(data.random_triple());
+    }
+    b.iter(move || {
+        let builder = base_layer.open_write().unwrap();
+
+        for triple in triples.iter() {
+            builder.add_string_triple(triple.clone()).unwrap();
+        }
+
+        builder.commit().unwrap();
+    });
+}
+
+#[bench]
+fn build_nonempty_child_layer_on_nonempty_base_layer(b: &mut Bencher) {
+    let dir = tempdir().unwrap();
+    let store = terminus_store::open_sync_directory_store(dir.path());
+
+    let seed = b"the quick brown fox jumped over ";
+    let rand = StdRng::from_seed(*seed);
+    let mut data = TestData::new(rand, 100, 25, 500);
+
+    let builder = store.create_base_layer().unwrap();
+
+    for _ in 0..1000 {
+        builder.add_string_triple(data.random_triple()).unwrap();
+    }
+    let base_layer = builder.commit().unwrap();
+
+    let num_triples = 1000;
+    let mut triples = Vec::with_capacity(num_triples);
+    for _ in 0..num_triples {
+        triples.push(data.random_triple());
+    }
+    b.iter(move || {
+        let builder = base_layer.open_write().unwrap();
+
+        for triple in triples.iter() {
+            builder.add_string_triple(triple.clone()).unwrap();
+        }
+
+        builder.commit().unwrap();
+    });
+}
