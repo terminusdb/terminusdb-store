f69b5a73e83e8e5b03e4571ae5238f5fe8202c23
Matthijs van Otterdijk <matthijs@terminusdb.com>
2021-03-24 15:11:35 +0100

    imprecise rollup upto implementation

--- src/storage/delta.rs
+++ src/storage/delta.rs
@@ -1,5 +1,4 @@
 use std::io;
-use std::sync::Arc;
 
 use crate::layer::builder::{build_indexes, TripleFileBuilder};
 use crate::layer::id_map::{construct_idmaps, construct_idmaps_upto};
@@ -9,7 +8,7 @@ use crate::structure::*;
 
 async fn safe_upto_bound<S:LayerStore>(
     store: &S,
-    layer: Arc<InternalLayer>,
+    layer: &InternalLayer,
     upto: [u32; 5]
 ) -> io::Result<[u32;5]> {
     if InternalLayerImpl::name(&*layer) == upto {
@@ -123,6 +122,70 @@ pub async fn delta_rollup<F: 'static + FileLoad + FileStore>(
         files.o_ps_adjacency_list_files.clone(),
         None,
         files.predicate_wavelet_tree_files.clone(),
+    )
+    .await
+}
+
+pub async fn imprecise_delta_rollup_upto<S:LayerStore, F: 'static + FileLoad + FileStore>(
+    store: &S,
+    layer: &InternalLayer,
+    upto: [u32; 5],
+    files: ChildLayerFiles<F>,
+) -> io::Result<()> {
+    let bound = safe_upto_bound(store, layer, upto).await?;
+    dictionary_rollup_upto(layer, bound, &files).await?;
+
+    let counts = layer.all_counts();
+
+    let mut pos_builder = TripleFileBuilder::new(
+        files.pos_s_p_adjacency_list_files.clone(),
+        files.pos_sp_o_adjacency_list_files.clone(),
+        counts.node_count,
+        counts.predicate_count,
+        counts.value_count,
+        Some(files.pos_subjects_file),
+    );
+
+    let mut neg_builder = TripleFileBuilder::new(
+        files.neg_s_p_adjacency_list_files.clone(),
+        files.neg_sp_o_adjacency_list_files.clone(),
+        counts.node_count,
+        counts.predicate_count,
+        counts.value_count,
+        Some(files.neg_subjects_file),
+    );
+
+    let additions = InternalTripleStackIterator::from_layer_stack(layer, bound)
+        .expect("bound not found")
+        .filter(|(sort, _)| *sort == TripleChange::Addition)
+        .map(|(_, t)| t);
+
+    let removals = InternalTripleStackIterator::from_layer_stack(layer, bound)
+        .expect("bound not found")
+        .filter(|(sort, _)| *sort == TripleChange::Removal)
+        .map(|(_, t)| t);
+
+    pos_builder.add_id_triples(additions).await?;
+    pos_builder.finalize().await?;
+
+    neg_builder.add_id_triples(removals).await?;
+    neg_builder.finalize().await?;
+
+    build_indexes(
+        files.pos_s_p_adjacency_list_files.clone(),
+        files.pos_sp_o_adjacency_list_files.clone(),
+        files.pos_o_ps_adjacency_list_files.clone(),
+        Some(files.pos_objects_file.clone()),
+        files.pos_predicate_wavelet_tree_files.clone(),
+    )
+    .await?;
+
+    build_indexes(
+        files.neg_s_p_adjacency_list_files.clone(),
+        files.neg_sp_o_adjacency_list_files.clone(),
+        files.neg_o_ps_adjacency_list_files.clone(),
+        Some(files.neg_objects_file.clone()),
+        files.neg_predicate_wavelet_tree_files.clone(),
     )
     .await
 }
